3fb079079bf4c20117005ea1b3f591c6396d9afc
Nevnet99 <luke-brannagan@hotmail.com>
2023-11-10 14:17:29 +0000

    feat: added animations

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,24 @@
 import { Inter } from "next/font/google";
+import { useReward } from "react-rewards";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const { reward, isAnimating } = useReward("rewardId", "emoji", {
+    emoji: ["🍔"],
+    elementCount: 1000,
+    spread: 360,
+    elementSize: 100,
+  });
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-center p-24 ${inter.className}`}
     >
-      <h1 className="text-9xl">Yes.</h1>
+      <button disabled={isAnimating} onClick={reward}>
+        <span id="rewardId" />
+        <h1 className="text-9xl">Yes.</h1>
+      </button>
     </main>
   );
 }
